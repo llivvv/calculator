4022bf84be1d153072e53097f3f2edd90531a5ae
llivvv <livialin2014@gmail.com>
2025-06-11 23:38:15 +0800

    added display digit on click function

--- script.js
+++ script.js
@@ -30,9 +30,39 @@ function operate(op, a, b) {
   }
 }
 
+function clickDisplay(numStr) {
+  if (prev == "num" || prev == "dec") {
+    display.innerText += numStr;
+  } else {
+    display.innerText = numStr;
+  }
+}
+
 let operator;
 let a;
 let b;
 
 // keep track of whether user's last press was a num or operator or decimal etc.
-let prev;
+let prev = null;
+let isDecAvail = true;
+
+const display = document.querySelector(".display");
+const clearBtn = document.querySelector(".btn-clear");
+const numBtns = document.querySelectorAll(".btn-num");
+numBtns.forEach((numBtn) => {
+  numBtn.addEventListener("click", () => {
+    clickDisplay(numBtn.innerText);
+    prev = "num";
+    clearBtn.innerText = "C";
+  });
+});
+
+const decBtn = document.querySelector(".btn-dec");
+decBtn.addEventListener("click", () => {
+  if (isDecAvail) {
+    clickDisplay(".");
+    prev = "dec";
+    isDecAvail = false;
+    clearBtn.innerText = "C";
+  }
+});
