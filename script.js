dbb65fd8a5ee8cb2d551503111d50c1e9fca45aa
llivvv <livialin2014@gmail.com>
2025-06-12 00:26:50 +0800

    add functions for extra btns

--- script.js
+++ script.js
@@ -33,14 +33,37 @@ function operate(op, a, b) {
 function clickDisplay(numStr) {
   if (prev == "num" || prev == "dec") {
     display.innerText += numStr;
+    arrNum[arrNum.length - 1] = parseFloat(display.innerText);
   } else {
     display.innerText = numStr;
+    arrNum.push(parseFloat(numStr));
   }
+}
+
+function intoPcntAndDisplay() {
+  //   console.log("here!");
+  //   console.log(arrNum.length - 1);
+  if (arrNum.length - 1 >= 0) {
+    console.log("here?");
+    arrNum[arrNum.length - 1] = arrNum[arrNum.length - 1] / 100;
+    display.innerText = arrNum[arrNum.length - 1];
+  }
+}
+
+function clear() {
+  arrNum = [];
+  arrOps = [];
+  prev = null;
+  isDecAvail = true;
+  clearBtn.innerText = "AC";
+  display.innerText = "0";
 }
 
 let operator;
 let a;
 let b;
+let arrNum = [];
+let arrOps = [];
 
 // keep track of whether user's last press was a num or operator or decimal etc.
 let prev = null;
@@ -48,6 +71,7 @@ let isDecAvail = true;
 
 const display = document.querySelector(".display");
 const clearBtn = document.querySelector(".btn-clear");
+clearBtn.addEventListener("click", clear);
 const numBtns = document.querySelectorAll(".btn-num");
 numBtns.forEach((numBtn) => {
   numBtn.addEventListener("click", () => {
@@ -66,3 +90,6 @@ decBtn.addEventListener("click", () => {
     clearBtn.innerText = "C";
   }
 });
+
+const pcntBtn = document.querySelector(".btn-pcnt");
+pcntBtn.addEventListener("click", intoPcntAndDisplay);
